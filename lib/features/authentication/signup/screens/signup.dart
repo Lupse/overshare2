ecaff9c8d2ac543805cb75b5267bcb7ffd0d2a5c
dejanazul <dejanazoel@gmail.com>
2024-09-22 20:04:14 +0700

    fck this shit

--- lib/features/authentication/signup/screens/signup.dart
+++ lib/features/authentication/signup/screens/signup.dart
@@ -18,243 +18,221 @@ class Signup extends StatelessWidget {
     final signUpController = Get.put(SignupController());
 
     return Scaffold(
-      backgroundColor: const Color(0xFF151515),
-      appBar: const PreferredSize(
-          preferredSize: Size(double.infinity, 60),
-          child: MyAppBar(backgroundColor: Color(0xFF151515))),
-      body: GestureDetector(
-        onTap: () => FocusScope.of(context).unfocus(),
-        child: SingleChildScrollView(
-          child: Container(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            width: 450,
-            decoration: const BoxDecoration(color: Color(0xFF151515)),
-            child: Form(
-              key: signupFormKey,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  // Title
-                  Padding(
-                    padding: const EdgeInsets.only(top: 60.0),
-                    child: Text(
-                      "Sign Up",
-                      style: GoogleFonts.josefinSans(
-                        textStyle: const TextStyle(
-                          fontSize: 34.35,
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
+        backgroundColor: const Color(0xFF151515),
+        appBar: const PreferredSize(
+            preferredSize: Size(double.infinity, 60),
+            child: MyAppBar(
+                backgroundColor: Color(0xFF151515),
+                leading: false,
+                withLeading: false)),
+        body: Form(
+          key: signupFormKey,
+          child: SingleChildScrollView(
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              width: 450,
+              decoration: const BoxDecoration(color: Color(0xFF151515)),
+              child: Form(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    // Title
+                    Padding(
+                      padding: const EdgeInsets.only(top: 60.0),
+                      child: Text(
+                        "Sign Up",
+                        style: GoogleFonts.josefinSans(
+                          textStyle: const TextStyle(
+                            fontSize: 34.35,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ),
                     ),
-                  ),
-          child: MyAppBar(backgroundColor: Color(0xFF151515),leading: false,withLeading: false,)),
-      body: SingleChildScrollView(
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          width: 450,
-          decoration: const BoxDecoration(color: Color(0xFF151515)),
-          child: Form(
-            key: formKey,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                // Title
-                Padding(
-                  padding: const EdgeInsets.only(top: 60.0),
-                  child: Text(
-                    "Sign Up",
-                    style: GoogleFonts.josefinSans(
-                      textStyle: const TextStyle(
-                        fontSize: 34.35,
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ),
 
-                  // Text
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 80.0),
-                    child: Text(
-                      "Register your account to use our special features",
-                      textAlign: TextAlign.center,
-                      style: GoogleFonts.josefinSans(
-                        textStyle: const TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
+                    // Text
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 80.0),
+                      child: Text(
+                        "Register your account to use our special features",
+                        textAlign: TextAlign.center,
+                        style: GoogleFonts.josefinSans(
+                          textStyle: const TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
                         ),
                       ),
                     ),
-                  ),
 
-                  // Email Textfield
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: SizedBox(
-                        width: 388,
-                        height: 61,
-                        child: MyTextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Username cannot be empty';
-                            }
-                            return null;
-                          },
-                          myController: signUpController.usernameController,
-                          text: 'Username',
-                          hide: false,
-                        )),
-                  ),
-
-                  // Email Textfield
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: SizedBox(
-                        width: 388,
-                        height: 61,
-                        child: MyTextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Email cannot be empty';
-                            }
-                            return null;
-                          },
-                          myController: signUpController.emailController,
-                          text: 'Email',
-                          hide: false,
-                        )),
-                  ),
-
-                  // Phone Textfield
-                  const Padding(
-                    padding: EdgeInsets.only(top: 20.0),
-                    child: SizedBox(
-                        width: 388, height: 61, child: PhoneNumberInput()),
-                  ),
-
-                  // Password Textfield
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: SizedBox(
-                        width: 388,
-                        height: 61,
-                        child: MyTextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Password cannot be empty';
-                            }
-                            return null;
-                          },
-                          myController: signUpController.passwordController,
-                          text: 'Password',
-                          hide: true,
-                        )),
-                  ),
-
-                  // Confirm Password Textfield
-                  Padding(
-                    padding: const EdgeInsets.only(top: 20.0),
-                    child: SizedBox(
-                        width: 388,
-                        height: 61,
-                        child: MyTextFormField(
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Password cannot be empty';
-                            } else if (value !=
-                                SignupController
-                                    .instance.passwordController.text) {
-                              return 'Password does not match';
-                            }
-                            return null;
-                          },
-                          text: 'Confirm Password',
-                          hide: true,
-                          myController:
-                              signUpController.confirmPasswordController,
-                        )),
-                  ),
-
-                  // SignUp Button
-                  Padding(
-                    padding: const EdgeInsets.only(top: 40.0),
-                    child: SizedBox(
-                        width: 388,
-                        height: 43,
-                        child: MyButton(
-                            text: 'Sign Up',
-                            onPressed: () {
-                              if (signupFormKey.currentState!.validate()) {
-                                //signup data
-                                final user = SignupModel(
-                                    username: signUpController
-                                        .usernameController.text
-                                        .trim(),
-                                    phone: signUpController.phoneController.text
-                                        .trim(),
-                                    email: signUpController.emailController.text
-                                        .trim(),
-                                    password: signUpController
-                                        .passwordController.text
-                                        .trim());
-                                signUpController.signupUserData(user);
-
-                                //signup auth
-                                SignupController.instance.signupUserAuth(
-                                    signUpController.emailController.text
-                                        .trim(),
-                                    signUpController.passwordController.text
-                                        .trim());
+                    // Email Textfield
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: SizedBox(
+                          width: 388,
+                          height: 61,
+                          child: MyTextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Username cannot be empty';
                               }
-                            })),
-                  ),
+                              return null;
+                            },
+                            myController: signUpController.usernameController,
+                            text: 'Username',
+                            hide: false,
+                          )),
+                    ),
 
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 17.0, right: 8),
-                        child: Text(
-                          "Already Have an Account?",
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.josefinSans(
-                            textStyle: const TextStyle(
-                              fontSize: 15,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.only(top: 18.0),
-                        child: InkWell(
-                          onTap: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => const LoginPage()));
-                          },
+                    // Email Textfield
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: SizedBox(
+                          width: 388,
+                          height: 61,
+                          child: MyTextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Email cannot be empty';
+                              }
+                              return null;
+                            },
+                            myController: signUpController.emailController,
+                            text: 'Email',
+                            hide: false,
+                          )),
+                    ),
+
+                    // Phone Textfield
+                    const Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                      child: SizedBox(
+                          width: 388, height: 61, child: PhoneNumberInput()),
+                    ),
+
+                    // Password Textfield
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: SizedBox(
+                          width: 388,
+                          height: 61,
+                          child: MyTextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password cannot be empty';
+                              }
+                              return null;
+                            },
+                            myController: signUpController.passwordController,
+                            text: 'Password',
+                            hide: true,
+                          )),
+                    ),
+
+                    // Confirm Password Textfield
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20.0),
+                      child: SizedBox(
+                          width: 388,
+                          height: 61,
+                          child: MyTextFormField(
+                            validator: (value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Password cannot be empty';
+                              } else if (value !=
+                                  SignupController
+                                      .instance.passwordController.text) {
+                                return 'Password does not match';
+                              }
+                              return null;
+                            },
+                            text: 'Confirm Password',
+                            hide: true,
+                            myController:
+                                signUpController.confirmPasswordController,
+                          )),
+                    ),
+
+                    // SignUp Button
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0),
+                      child: SizedBox(
+                          width: 388,
+                          height: 43,
+                          child: MyButton(
+                              text: 'Sign Up',
+                              onPressed: () {
+                                if (signupFormKey.currentState!.validate()) {
+                                  //signup data
+                                  final user = SignupModel(
+                                      username: signUpController
+                                          .usernameController.text
+                                          .trim(),
+                                      phone: signUpController
+                                          .phoneController.text
+                                          .trim(),
+                                      email: signUpController
+                                          .emailController.text
+                                          .trim(),
+                                      password: signUpController
+                                          .passwordController.text
+                                          .trim());
+                                  signUpController.signupUserData(user);
+
+                                  //signup auth
+                                  SignupController.instance.signupUserAuth(
+                                      signUpController.emailController.text
+                                          .trim(),
+                                      signUpController.passwordController.text
+                                          .trim());
+                                }
+                              })),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 17.0, right: 8),
                           child: Text(
-                            'Log In',
+                            "Already Have an Account?",
+                            textAlign: TextAlign.center,
                             style: GoogleFonts.josefinSans(
-                              color: const Color(0xFFF07A00),
-                              fontSize: 16,
-                              fontWeight: FontWeight.w700,
+                              textStyle: const TextStyle(
+                                fontSize: 15,
+                                color: Colors.white,
+                              ),
                             ),
                           ),
                         ),
-                      )
-                    ],
-                  ),
-                ],
+                        Padding(
+                          padding: const EdgeInsets.only(top: 18.0),
+                          child: InkWell(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => const LoginPage()));
+                            },
+                            child: Text(
+                              'Log In',
+                              style: GoogleFonts.josefinSans(
+                                color: const Color(0xFFF07A00),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w700,
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-      ),
-    );
+        ));
   }
 }
